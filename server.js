d0b3b81cdcb0750667b4112de64b7b2b475fc2cf
arnold tan <arnoldtan100@gmail.com>
2020-03-17 10:11:24 +0700

    server js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(3000, () => {
-    console.log('Server is running...');
+app.get('/',(req,res)=>{
+    res.send("Deploy this");
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
