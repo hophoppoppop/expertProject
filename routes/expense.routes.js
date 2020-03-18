8ef30622051372f5ff3933191b80c359cefd56a8
arnold tan <arnoldtan100@gmail.com>
2020-03-18 11:09:04 +0700

    push

--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -1,12 +1,6 @@
 const expense = require('../controllers/expense.controller');
 
 const routes = (app) => {
-    // app.get('/pets', pets.getAll);
-    // app.get('/pets/:id', pets.get);
-    // app.get('/pets', pets.findAll);
-    // app.put('/pets/:id',pets.update);
-    // app.get('/pets-redis', pets.findAllRedis);
-
     app.post('/expense', expense.create);
     app.get('/expense', expense.get);
     app.delete('/expense',expense.remove);
